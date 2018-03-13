4a46901c13610ba2477faf27c58faa7ff8a62967
kit-name <kit@aladinfun.com>
2018-03-13 11:15:33 +0800

    1

--- server_date.go
+++ server_date.go
@@ -14,17 +14,18 @@ func check(e error)  {
 }
 func main() {
 	URL := "http://planettest.boomegg.cn/testenv/alipay/m/common/bytesConfig"
-	resp, err := http.Get(URL)
+	resp, err := http.Get(URL)    //获响应报文
 	check(err)
 	fmt.Println("resp: ", resp)
-	//resp.Body.Close()
+	defer resp.Body.Close()		//函数执行完成后执行关闭收尾工作
 
-	date, _ := ioutil.ReadAll(resp.Body)
-	//check(err1)
+	date, err1 := ioutil.ReadAll(resp.Body) //读取服务器发送的数据
+	check(err1)
 	str := string(date)
 	fmt.Println("str: ",str )
-	var dat map[string]interface{}
 
+	//解析json结构数据第一层嵌套 str到map
+	var dat map[string]interface{}
 	json.Unmarshal(date,&dat)
 	fmt.Println("dat: ",dat)
 	fmt.Println("\n===========json_dat[]===========")
@@ -32,13 +33,15 @@ func main() {
 		fmt.Printf("%5s --> %s\n",k,v)
 	}
 
-	pot := dat["_d"].(map[string]interface{})
+	//解析json结构数据第二层嵌套 str到map
+	pot := dat["_d"].(map[string]interface{})//将dat键值为“_d”的数据作为json进行解析
 	fmt.Println("pot: ",pot)
 	fmt.Println("\n===========json_pot[_d]===========")
 	for k, v := range pot{
 		fmt.Printf("%5s --> %s\n",k,v)
 	}
 
+	//解析json结构数据第三层嵌套 str到map
 	list :=pot["list"].(map[string]interface{})
 	fmt.Println("\n===========json_list[list]===========")
 	for k, v := range list{
